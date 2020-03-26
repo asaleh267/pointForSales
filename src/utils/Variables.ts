f3f87620f4ec74c3935b751459d3308f0a5b472c
aseelsaleh <aseel.saleh2611@gmail.com>
2020-03-26 13:01:26 +0200

    Define variables

---
+++ src/utils/Variables.ts
@@ -0,0 +1,18 @@
+export const colors = {
+    darkGrey: '#34495e',
+    darkGreen: '#1abc9c',
+    lightGreen: '#2ecc71',
+    blue: '#3498db',
+    purple: '#9b59b6',
+    orange: '#e67e22',
+    red: '#e74c3c',
+    yellow: '#f1c512'
+};
+
+export const fontSize = {
+  larger: 56,
+  large: 20,
+  normal: 14,
+  small: 16,
+  smaller: 12
+};
